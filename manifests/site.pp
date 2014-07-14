451681106f3d081a5cbc68a609477ffcf712b5de
Felipe <falmeida1988@gmail.com>
2014-07-14 02:46:06 -0300

    nit commit.

---
+++ manifests/site.pp
@@ -0,0 +1,92 @@
+
+$username = 'vagrant'
+
+Exec { 
+  path => [ "/bin/", "/sbin/" , "/usr/bin/", "/usr/sbin/","/usr/local/bin" ]
+} 
+
+# a command to run system updates
+exec { 'sys_update':
+  command => "apt-get update --fix-missing",
+}
+
+class git {
+  package{'git-core':
+    ensure => 'installed',
+    require => Exec['sys_update'],
+  }
+}
+
+class basic_gui{
+  package{'xfce':
+    require => Exec['sys_update'],
+    ensure => 'installed'       
+  } ->
+  package {'lightdm':
+    ensure => 'installed',    
+  } ->
+  package {'lightdm-gtk-greeter':
+    ensure => 'installed',
+  } ->
+  file {'/etc/lightdm/lightdm.conf':
+    ensure => 'present',
+    owner => 'root',
+    content => "[SeatDefaults] \n greeter-session=lightdm-gtk-greeter\n",
+  } ->
+  service {'lightdm':
+    ensure => 'running',
+    enable => true,
+  } 
+}
+
+class fix_broken{
+  exec{'fix-broken-command':
+    command => 'apt-get -f install --assume-yes',
+  }
+}
+
+class chrome{
+  require fix_broken
+  exec{'download chrome deb':
+    command => 'wget https://dl.google.com/linux/direct/google-chrome-stable_current_i386.deb',
+    cwd => "/home/${username}/Downloads",
+    creates => "/home/${username}/Downloads/google-chrome-stable_current_i386.deb",
+  } ->
+  exec{'install chrome':
+    command => 'dpkg -i google-chrome-stable_current_i386.deb',
+    cwd => "/home/${username}/Downloads",
+    creates => "/usr/bin/google-chrome",
+  } -> 
+  exec{'add shortcuts to desktop':
+    command => "ln -s /usr/bin/google-chrome /home/${username}/Desktop/google-chrome",
+    creates => "/home/${username}/Desktop/google-chrome",
+  } ->
+  exec{'set permissions':
+    command =>"chmod +x /home/${username}/Desktop/*",
+    provider => 'shell',
+  }
+}
+
+class r_stuff{
+  package{'r-base':
+    require => Exec['sys_update'],
+    ensure => 'installed'
+  } ->
+  exec{'dl rstudio':
+    command => 'wget http://download1.rstudio.org/rstudio-0.98.953-i386.deb',
+    cwd => "/home/${username}",
+    creates => "/home/${username}/rstudio-0.98.953-i386.deb",
+    user => "${username}",
+  } ->
+  exec{'install rstudio':
+    require => Class['fix_broken'],  
+    command => 'dpkg -i http://download1.rstudio.org/rstudio-0.98.953-i386.deb',
+  }
+  
+}
+
+include git
+include fix_broken
+include chrome
+include basic_gui
+include r_stuff
